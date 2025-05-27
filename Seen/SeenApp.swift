eb1e073ac5880386ad6bc83e7d06d039400d51ba
Alok Madan <amadan656@gmail.com>
2025-05-26 21:59:07 -0400

    Initial Commit

---
+++ Seen/SeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeenApp.swift
+//  Seen
+//
+//  Created by Alok Madan on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
